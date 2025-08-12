88fb1e06e2ad19ad344b3af10be96b38e06c2111
24ftt1829 <24FTT1829@student.pb.edu.bn>
2025-08-12 20:16:50 +0800

    Delete ComputeExpression.java

--- ComputeExpression.java
+++
@@ -1,7 +0,0 @@
-public class ComputeExpression {
-    public static void main(String[] args) {
-        
-        System.out.println("Hello, world!");
-        System.out.println("Welcome to AD2204 Introduction to Programming");
-    }
-}
